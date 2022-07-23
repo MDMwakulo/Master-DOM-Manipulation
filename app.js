1902cb10fc7250ac7d2b0efdfdebc2e9c140ec7d
MDMwakulo <mdmwakulo@gmail.com>
2022-07-23 11:17:38 +0300

    Added styles.css

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 // DOM Manipulation
 
-const title = document.querySelector('#main-heading');
-title.style.color = 'red';
-console.log(title);
+// 1) Styling Elements
+
+// const title = document.querySelector('#main-heading');
+// title.style.color = 'red';
+// console.log(title);
 
 // Applying syles to multiple elements
-const listItems = document.querySelectorAll('.list-items')
+// const listItems = document.querySelectorAll('.list-items')
 
-for(let i = 0; i < listItems.length; i++){
-  listItems[i].style.color = 'purple';
-}
+// for(let i = 0; i < listItems.length; i++){
+//   listItems[i].style.color = 'purple';
+// }
+
+// 2) Creating Elements
+
+const ul = document.querySelector('ul');
+const li = document.createElement('li');
+
+// Adding Element
+
+ul.append(li)
+
+// Modifying the text
+
+li.innerText = 'Papa Johns';
+li.setAttribute('id', 'main-heading')
+li.removeAttribute('id')
+
+// Getting Attributes
+
+const title = document.querySelector('#main-heading');
+
+console.log(title.getAttribute('id'))
