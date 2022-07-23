7535c4fc4f2d0b677d18217451b4effbcc7c3874
MDMwakulo <mdmwakulo@gmail.com>
2022-07-23 22:50:08 +0300

    Using children instead of childNodes

--- app.js
+++ app.js
@@ -15,16 +15,16 @@
 // console.log(html.parentElement);
 
 // Child Node Traversal
-let ul = document.querySelector('ul');
-console.log(ul.childNodes)
-console.log(ul.firstChild)
-console.log(ul.lastChild)
-
-ul.childNodes[1].style.backgroundColor = 'red';
-ul.childNodes[1].style.color = 'white';
-
 // let ul = document.querySelector('ul');
-// console.log(ul.children)
-// console.log(ul.firstElementChild);// Here you get the exact first child elememt
-// console.log(ul.lastElementChild); // Here you get the exact last child elememt
+// console.log(ul.childNodes)
+// console.log(ul.firstChild)
+// console.log(ul.lastChild)
+
+// ul.childNodes[1].style.backgroundColor = 'red';
+// ul.childNodes[1].style.color = 'white';
+
+let ul = document.querySelector('ul');
+console.log(ul.children)
+console.log(ul.firstElementChild);// Here you get the exact first child elememt
+console.log(ul.lastElementChild); // Here you get the exact last child elememt
 // Sibling Node Traversal
