a0bcd17d19d912bfde5c0e06364caaf2405e51cc
MDMwakulo <mdmwakulo@gmail.com>
2022-07-23 10:38:36 +0300

    changed all list-items color to purple in app.js

--- app.js
+++ app.js
@@ -2,3 +2,11 @@
 
 const title = document.querySelector('#main-heading');
 title.style.color = 'red';
+console.log(title);
+
+// Applying syles to multiple elements
+const listItems = document.querySelectorAll('.list-items')
+
+for(let i = 0; i < listItems.length; i++){
+  listItems[i].style.color = 'purple';
+}
