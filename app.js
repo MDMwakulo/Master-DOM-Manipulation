151185a44a73592439aef19399a9615405e20d7d
MDMwakulo <mdmwakulo@gmail.com>
2022-07-23 18:51:45 +0300

    Creating element in app.js

--- app.js
+++ app.js
@@ -1,35 +1,8 @@
 // DOM Manipulation
 
-// 1) Styling Elements
-
-// const title = document.querySelector('#main-heading');
-// title.style.color = 'red';
-// console.log(title);
-
-// Applying syles to multiple elements
-// const listItems = document.querySelectorAll('.list-items')
-
-// for(let i = 0; i < listItems.length; i++){
-//   listItems[i].style.color = 'purple';
-// }
-
-// 2) Creating Elements
-
 const ul = document.querySelector('ul');
 const li = document.createElement('li');
 
-// Adding Element
-
-ul.append(li)
-
-// Modifying the text
-
 li.innerText = 'Papa Johns';
-
-// Modifying Attributes and Classes
-li.classList.add('list-items');// This adds class of List-items to our li
-
-console.log(li.classList.contains('list-items'));
-
-// Removing elements
-li.remove();
+li.classList.add('list-items');
+ul.append(li);
