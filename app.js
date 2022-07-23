3809bb9c263b82cecbc8b4ecb1b2d07953348c52
MDMwakulo <mdmwakulo@gmail.com>
2022-07-23 08:27:16 +0300

    Updated app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// DOM Manipulation
+
+// getElementById()
+const title = document.getElementById('main-heading');
+console.log(title);
+
+// getElementByClassName()
+// const listItems = document.getElementsByClassName('list-items');
+// console.log(listItems);
+
+// getElementByTagName()
+const listItems = document.getElementsByTagName('li');
+console.log(listItems);
