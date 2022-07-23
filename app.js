eae3323b9d1e95aeb1aa4af9ea6997514d4f69ff
MDMwakulo <mdmwakulo@gmail.com>
2022-07-23 10:26:10 +0300

    changed main-heading to red in app.js file

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
 // DOM Manipulation
 
-// getElementById()
-const title = document.getElementById('main-heading');
-console.log(title);
-
-// getElementByClassName()
-// const listItems = document.getElementsByClassName('list-items');
-// console.log(listItems);
-
-// getElementByTagName()
-const listItems = document.getElementsByTagName('li');
-console.log(listItems);
+const title = document.querySelector('#main-heading');
+title.style.color = 'red';
