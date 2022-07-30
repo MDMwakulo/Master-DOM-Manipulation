02fdd9ed7927fd0ab5c7f588b8f9f534fdcad5a6
MDMwakulo <mdmwakulo@gmail.com>
2022-07-30 11:53:16 +0300

    Event Propagation

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 // DOM Manipulation
-const revealBtn = document.querySelector('.reveal-btn');
-const hiddenContent = document.querySelector('.hidden-content');
 
-function revealContent(){
-  if(hiddenContent.classList.contains('reveal-btn')){
-    hiddenContent.classList.remove('reveal-btn');
-  } else {
-    hiddenContent.classList.add('reveal-btn');
-  }
-}
+// Event Propagation
 
-revealBtn.addEventListener('click', revealContent);
+window.addEventListener('click', function() {
+  console.log('Window');
+}, false); // the true boolean value is to campture the event capturing
+
+document.addEventListener('click', function() {
+  console.log('Document');
+}, false);
+
+document.querySelector('.div2').addEventListener('click', function() {
+  console.log('Div 2');
+}, false); //one the first click of the button div2 will fire but on the second click div2 will not fire.
+
+document.querySelector('.div1').addEventListener('click', function() {
+  console.log('Div 1');
+}, false);
+
+document.querySelector('.button').addEventListener('click', function(e) {
+  e.preventDefault();
+  console.log(e.target.innerText = 'Clicked');
+}, false);
