b452afb19d5123446f2cde88807563fe063e3fc4
MDMwakulo <mdmwakulo@gmail.com>
2022-08-04 22:01:50 +0300

    Random Quote Generator

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
-// DOM Manipulation
+// Variables
 
-// Event Delegation
+let btn = document.querySelector('#new-quote')
+let quote = document.querySelector('.quote');
+let person = document.querySelector('.person');
 
-document.querySelector('#sports').addEventListener('click', function(e) {
-  const target = e.target;
+const quotes = [
+  {
+    quote: `"At his best, man is the noblest of all animals; separated from law and justice he is the worst."`,
+    person: `Aristotle`
+  },
+  {
+    quote:`"It does not matter how slowly you go as long as you do not stop."`,
+    person:`Confucius`
+  },
+  {
+    quote: `"Remember that not getting what you want is sometimes a wonderful stroke of luck"`,
+    person: `Dalai Lama`
+  },
+  {
+    quote: `"The journey of thousand miles begins with one step"`,
+    person: `Lao Tzu`
+  }
+]
 
-  console.log(target.textContent + ' is clicked.');
+btn.addEventListener('click', function() {
+  let random = Math.floor(Math.random() * quotes.length);
+  let check = undefined;
+  
+  if(random !== check){
+    check = random;
+    console.log(check)
+    quote.innerText = quotes[check].quote;
+    person.innerText = quotes[check].person;
+  } else {
+    random = Math.floor(Math.random() * quotes.length);
+    check = random
+    console.log(check);
+    quote.innerText = quotes[check].quote;
+    person.innerText = quotes[check].person;
+  }
 
-  if(target.matches('li')) {
-    target.style.backgroundColor = 'grey';
-    target.style.color = 'white';
-  } 
-});
-// Adding a new list item
-const sports = document.querySelector('#sports');
-const newSport = document.createElement('li');
-
-newSport.innerText = 'Rugby';
-newSport.setAttribute('id', 'rugby');
-
-sports.append(newSport);
+})
