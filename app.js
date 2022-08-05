e3abdf22d28745a154ed01e84da3114b3494e0d5
MDMwakulo <mdmwakulo@gmail.com>
2022-08-05 20:31:03 +0300

    Modal Project

--- app.js
+++ app.js
@@ -1,43 +1,23 @@
 // Variables
 
-let btn = document.querySelector('#new-quote')
-let quote = document.querySelector('.quote');
-let person = document.querySelector('.person');
+let openBtn = document.querySelector('#open-btn');
+let modalContainer = document.querySelector('#modal-container');
+let closeBtn = document.querySelector('#close-btn');
 
-const quotes = [
-  {
-    quote: `"At his best, man is the noblest of all animals; separated from law and justice he is the worst."`,
-    person: `Aristotle`
-  },
-  {
-    quote:`"It does not matter how slowly you go as long as you do not stop."`,
-    person:`Confucius`
-  },
-  {
-    quote: `"Remember that not getting what you want is sometimes a wonderful stroke of luck"`,
-    person: `Dalai Lama`
-  },
-  {
-    quote: `"The journey of thousand miles begins with one step"`,
-    person: `Lao Tzu`
+// Event Listeners
+
+openBtn.addEventListener('click', function() {
+  modalContainer.style.display = 'block';
+
+})
+
+closeBtn.addEventListener('click', function() {
+  modalContainer.style.display = 'none';
+})
+
+window.addEventListener('click', function(e) {
+  const target = e.target;
+  if(target === modalContainer){
+    modalContainer.style.display = 'none';
   }
-]
-
-btn.addEventListener('click', function() {
-  let random = Math.floor(Math.random() * quotes.length);
-  let check = undefined;
-  
-  if(random !== check){
-    check = random;
-    console.log(check)
-    quote.innerText = quotes[check].quote;
-    person.innerText = quotes[check].person;
-  } else {
-    random = Math.floor(Math.random() * quotes.length);
-    check = random
-    console.log(check);
-    quote.innerText = quotes[check].quote;
-    person.innerText = quotes[check].person;
-  }
-
 })
